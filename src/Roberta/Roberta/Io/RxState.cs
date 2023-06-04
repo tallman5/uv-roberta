d7fbe97b4b2e54875860120fee57f881badb3979
tallman5 <tallman5@users.noreply.github.com>
2023-06-04 17:15:58 -0400

    adding Rx component

---
+++ src/Roberta/Roberta/Io/RxState.cs
@@ -0,0 +1,248 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Roberta.Io
+{
+    public class RxState : ItemState
+    {
+        private int _Channel01;
+        [DataMember]
+        public int Channel01
+        {
+            get { return this._Channel01; }
+            set
+            {
+                if (value != this._Channel01)
+                {
+                    this._Channel01 = value;
+                    RaisePropertyChanged(nameof(Channel01));
+                }
+            }
+        }
+
+        private int _Channel02;
+        [DataMember]
+        public int Channel02
+        {
+            get { return this._Channel02; }
+            set
+            {
+                if (value != this._Channel02)
+                {
+                    this._Channel02 = value;
+                    RaisePropertyChanged(nameof(Channel02));
+                }
+            }
+        }
+
+        private int _Channel03;
+        [DataMember]
+        public int Channel03
+        {
+            get { return this._Channel03; }
+            set
+            {
+                if (value != this._Channel03)
+                {
+                    this._Channel03 = value;
+                    RaisePropertyChanged(nameof(Channel03));
+                }
+            }
+        }
+
+        private int _Channel04;
+        [DataMember]
+        public int Channel04
+        {
+            get { return this._Channel04; }
+            set
+            {
+                if (value != this._Channel04)
+                {
+                    this._Channel04 = value;
+                    RaisePropertyChanged(nameof(Channel04));
+                }
+            }
+        }
+
+        private int _Channel05;
+        [DataMember]
+        public int Channel05
+        {
+            get { return this._Channel05; }
+            set
+            {
+                if (value != this._Channel05)
+                {
+                    this._Channel05 = value;
+                    RaisePropertyChanged(nameof(Channel05));
+                }
+            }
+        }
+
+        private int _Channel06;
+        [DataMember]
+        public int Channel06
+        {
+            get { return this._Channel06; }
+            set
+            {
+                if (value != this._Channel06)
+                {
+                    this._Channel06 = value;
+                    RaisePropertyChanged(nameof(Channel06));
+                }
+            }
+        }
+
+        private int _Channel07;
+        [DataMember]
+        public int Channel07
+        {
+            get { return this._Channel07; }
+            set
+            {
+                if (value != this._Channel07)
+                {
+                    this._Channel07 = value;
+                    RaisePropertyChanged(nameof(Channel07));
+                }
+            }
+        }
+
+        private int _Channel08;
+        [DataMember]
+        public int Channel08
+        {
+            get { return this._Channel08; }
+            set
+            {
+                if (value != this._Channel08)
+                {
+                    this._Channel08 = value;
+                    RaisePropertyChanged(nameof(Channel08));
+                }
+            }
+        }
+
+        private int _Channel09;
+        [DataMember]
+        public int Channel09
+        {
+            get { return this._Channel09; }
+            set
+            {
+                if (value != this._Channel09)
+                {
+                    this._Channel09 = value;
+                    RaisePropertyChanged(nameof(Channel09));
+                }
+            }
+        }
+
+        private int _Channel10;
+        [DataMember]
+        public int Channel10
+        {
+            get { return this._Channel10; }
+            set
+            {
+                if (value != this._Channel10)
+                {
+                    this._Channel10 = value;
+                    RaisePropertyChanged(nameof(Channel10));
+                }
+            }
+        }
+
+        private int _Channel11;
+        [DataMember]
+        public int Channel11
+        {
+            get { return this._Channel11; }
+            set
+            {
+                if (value != this._Channel11)
+                {
+                    this._Channel11 = value;
+                    RaisePropertyChanged(nameof(Channel11));
+                }
+            }
+        }
+
+        private int _Channel12;
+        [DataMember]
+        public int Channel12
+        {
+            get { return this._Channel12; }
+            set
+            {
+                if (value != this._Channel12)
+                {
+                    this._Channel12 = value;
+                    RaisePropertyChanged(nameof(Channel12));
+                }
+            }
+        }
+
+        private int _Channel13;
+        [DataMember]
+        public int Channel13
+        {
+            get { return this._Channel13; }
+            set
+            {
+                if (value != this._Channel13)
+                {
+                    this._Channel13 = value;
+                    RaisePropertyChanged(nameof(Channel13));
+                }
+            }
+        }
+
+        private int _Channel14;
+        [DataMember]
+        public int Channel14
+        {
+            get { return this._Channel14; }
+            set
+            {
+                if (value != this._Channel14)
+                {
+                    this._Channel14 = value;
+                    RaisePropertyChanged(nameof(Channel14));
+                }
+            }
+        }
+
+        private int _Channel15;
+        [DataMember]
+        public int Channel15
+        {
+            get { return this._Channel15; }
+            set
+            {
+                if (value != this._Channel15)
+                {
+                    this._Channel15 = value;
+                    RaisePropertyChanged(nameof(Channel15));
+                }
+            }
+        }
+
+        private int _Channel16;
+        [DataMember]
+        public int Channel16
+        {
+            get { return this._Channel16; }
+            set
+            {
+                if (value != this._Channel16)
+                {
+                    this._Channel16 = value;
+                    RaisePropertyChanged(nameof(Channel16));
+                }
+            }
+        }
+    }
+}
