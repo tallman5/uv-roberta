c00524498f2db6c12c57922ae937f65ce2a3f52f
tallman5 <tallman5@users.noreply.github.com>
2024-01-18 20:07:56 -0500

    updating script

--- bootstrap/pi-ros.sh
+++ bootstrap/pi-ros.sh
@@ -4,22 +4,12 @@ cd ~
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install software-properties-common -y
+# locales
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+sudo apt install software-properties-common
 sudo add-apt-repository universe
-
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-sudo apt update && sudo apt install ros-dev-tools -y
-
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install ros-iron-desktop
-
-sudo apt install ros-iron-ros-base
-
-# Replace ".bash" with your shell if you're not using bash
-# Possible values are: setup.bash, setup.sh, setup.zsh
-source /opt/ros/iron/setup.bash
